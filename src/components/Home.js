c1ee11d1eb99b2b0368e44c2b0c2fdc8d41d9c30
Gamalliel Sharon <you@example.com>
2021-07-05 14:59:32 +0700

    using props and state

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import BlogList from '../BlogList'
 
 const Home = () => {
 
-    const [blogs, setBlog] = useState([
+    const [blogs, setBlogs] = useState([
         {title: "My New Websote", body: 'lorem ipsum...', author: "mario", id:1},
         {title: "Hello World", body: 'lorem ipsum...', author: "Luigi", id:2},
         {title: "Web Dev Journey", body: 'lorem ipsum...', author: "Wario", id:3}
     ])
 
+    const [name, setName] = useState("Mario")
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs)
+    }
+
+    useEffect(() => {
+        console.log('use effect ran')
+        console.log(name)
+    }, [name])
+
     return ( 
         <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>{blog.author}</p>
-                </div>
-            ))}
+            <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/>
+            <button onClick={() => setName("luigi")}>Change Name</button>
+            <p>{name}</p>
         </div>
      );
 }
