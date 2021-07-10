d1786eb0c301ba7e6fe0cd8eeb66998ca728dfb5
Gamalliel Sharon <you@example.com>
2021-07-10 20:20:13 +0700

    adding blog details page

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 const BlogList = ({blogs, title}) => {
     // OPSI PERTAMA CUKUP MASUKKAN SEBAGAI PARAMETER DENGAN KURUNG KURAWAL YANG DIBUTUHKAN
     // OPSI KEDUA ADALAH DEKLARASIKAN SATU SATU SEPERTI DIBAWAH
@@ -9,8 +10,10 @@ const BlogList = ({blogs, title}) => {
             <h2>{title}</h2>
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written by {blog.author}</p>
+                    <Link to={`/blogs/${blog.id}`}>
+                        <h2>{blog.title}</h2>
+                        <p>Written by {blog.author}</p>
+                    </Link>
                 </div>
             ))}
         </div>
--- src/useFetch.js
+++ src/useFetch.js
@@ -7,7 +7,9 @@ const useFetch = (url) => {
     const [error, setError] = useState(null)
 
     useEffect(() => {
-        fetch(url)
+        const abortCont = new AbortController();
+
+        fetch(url, { signal: abortCont.signal })
         .then(res => {
             if(!res.ok){
                 throw Error('Could not fetch the data for that resource');
@@ -20,9 +22,15 @@ const useFetch = (url) => {
             setError(null)
         })
         .catch(err => {
-            setError(err.message)
-            setIsPending(false)
+            if(err.name === 'AbortError'){
+                console.log('Fetch Aborted')
+            }else{
+                setError(err.message)
+                setIsPending(false)
+            }
         })
+
+        return () => abortCont.abort()
     }, [url])
 
     return {data, isPendng, error}
